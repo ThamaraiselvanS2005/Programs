21ded3e5b1b8d7324fd0de98c173872cbc06c920
ThamaraiselvanS2005 <s.thamaraiselvan2005@gmail.com>
2025-03-27 10:22:45 +0530

    modified

---
+++ Swap.java
@@ -0,0 +1,34 @@
+package Programs;
+
+import java.util.Scanner;
+
+public class Swap {
+    public static void main (String args[]) {
+
+//
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter number 1 = ");
+        int num1 = scan.nextInt();
+        System.out.println("enter number 2 = ");
+        int num2 = scan .nextInt();
+        System.out.println("before swap");
+        System.out.println(num1 +","+ num2);
+//        int temp;
+//        temp = num1;
+//        num1 = num2;
+//        num2 = temp;
+
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+
+
+
+        System.out.println("after swap");
+        System.out.println(num1 +","+ num2);
+
+
+
+    }
+
+}
