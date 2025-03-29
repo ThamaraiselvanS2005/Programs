b2634f94ee29afe5ccfc08a12fdfae17befda439
ThamaraiselvanS2005 <s.thamaraiselvan2005@gmail.com>
2025-03-29 21:29:53 +0530

    again added

---
+++ Lc75sortColour.java
@@ -0,0 +1,47 @@
+package Programs;
+
+
+import java.util.Arrays;
+
+public class Lc75sortColour {
+public static void main(String[] args) {
+    int[] nums  = {1,2,0,1,0,2};
+    sortColors(nums);
+}
+public static void sortColors(int[] nums) {
+    //Arrays.sort(nums);
+    int start = 0, mid = 0 , end = nums.length-1;
+    while(mid <= end){
+        if(nums[mid]==0)
+        {
+            swap(nums,start,mid);
+            start++;
+            mid++;
+
+        }
+        else if(nums[mid]==1){
+            mid++;
+        }
+        else{
+            swap(nums,end,mid);
+            end--;
+        }
+    }
+    System.out.println(Arrays.toString(nums));
+}
+
+private static void swap(int[] nums ,int i , int j) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
+}
+/*
+public static void sortColors(int[] nums) {
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
+ */
+
